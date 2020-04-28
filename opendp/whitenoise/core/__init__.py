6b5bd683f6089edc7f2d94905d9b8a52edae1aa6
eedeleon <31962564+eedeleon@users.noreply.github.com>
2020-04-28 00:18:56 -0400

    Update __init__.py

--- opendp/whitenoise/core/__init__.py
+++ opendp/whitenoise/core/__init__.py
@@ -1,7 +1,7 @@
 from .base import *
 
 # The full namespace is used because the file location comes from an external repo
-# components.py is generated from rust 
-# from scripts/code_generation.py in opendifferentialprivacy/whitenoise-core
+# components.py is generated from scripts/code_generation.py
+# in opendifferentialprivacy/whitenoise-core
 from opendp.whitenoise.core.components import *
 
