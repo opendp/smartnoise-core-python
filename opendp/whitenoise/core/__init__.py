4a6aecebc91a3ded6d4f02c45b1631f77cd73196
Raman Prasad <raman_prasad@harvard.edu>
2020-05-05 04:13:25 -0400

    docs

--- opendp/whitenoise/core/__init__.py
+++
@@ -1,7 +0,0 @@
-from .base import *
-
-# The full namespace is used because the file location comes from an external script
-# components.py is generated from scripts/code_generation.py
-# in opendifferentialprivacy/whitenoise-core-python
-from opendp.whitenoise.core.components import *
-
--- scripts/build_wheels.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-bash scripts/clean.sh
-
-python setup.py sdist -d ./wheelhouse
-python scripts/code_generation.py
-python setup.py bdist_wheel -d ./wheelhouse
