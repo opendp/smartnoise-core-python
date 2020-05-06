cc992e09a72a7c58e731d9cc5a9dc00fe57052e9
Shoeboxam <shoeboxam@gmail.com>
2020-05-06 01:33:36 -0400

    adjust wheel-builders

--- scripts/build_manylinux_wheels.sh
+++ scripts/build_manylinux_wheels.sh
@@ -5,17 +5,12 @@
 # exit immediately upon failure, print commands while running
 set -e -x
 
-cd /io/bindings-python/
-# python3 -m pip install -r /io/bindings-python/requirements.txt
-# python3 /io/bindings-python/setup.py -d wheelhouse/
-# Compile wheels
-
 
 yum -y install openssl-devel
 
 # install protoc if not exists
 if ! [ -x "$(command -v protoc)" ]; then
-  	PROTOC_ZIP=protoc-$1-$2
+  PROTOC_ZIP=protoc-$1-$2
 	curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v$1/$PROTOC_ZIP
 	unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
 	unzip -o $PROTOC_ZIP -d /usr/local 'include/*'
@@ -23,7 +18,7 @@ if ! [ -x "$(command -v protoc)" ]; then
 fi
 
 if ! [ -x "$(command -v cargo)" ]; then
-  	curl https://sh.rustup.rs -sSf | sh -s -- -y
+  curl https://sh.rustup.rs -sSf | sh -s -- -y
 	export PATH="${HOME}/.cargo/bin:${PATH}"
 fi
 
@@ -36,14 +31,12 @@ fi
 # export makes the updated path available in subprocesses
 export PATH="/opt/python/cp38-cp38/bin:$PATH"
 
-pip install -r requirements.txt
-python setup.py sdist bdist_wheel -d /io/wheelhouse # ./ -w wheelhouse # bdist_wheel -d /wheelhouse
+pip install -r /io/requirements.txt
 
-python ../setup.py sdist -d /io/wheelhouse
-python code_generation.py
-python ../setup.py bdist_wheel -d /io/wheelhouse
+#python ../setup.py sdist -d /io/wheelhouse
+python /io/scripts/code_generation.py
+python /io/setup.py bdist_wheel -d /io/wheelhouse
 
 for whl in /io/wheelhouse/*.whl; do
-	auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
-done
-
+	auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/;
+done;
--- scripts/build_wheels.sh
+++ scripts/build_wheels.sh
@@ -6,3 +6,11 @@ bash scripts/clean.sh
 #python setup.py sdist -d ./wheelhouse
 python scripts/code_generation.py
 python setup.py bdist_wheel -d ./wheelhouse
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  echo "not implemented"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  for whl in ./wheelhouse/*.whl; do
+    delocate-wheel -v "$whl"
+  done
+fi
