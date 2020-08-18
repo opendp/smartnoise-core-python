29d78d528115302704f886c7eb049cf409294c7c
Shoeboxam <shoeboxam@gmail.com>
2020-08-18 00:30:02 -0500

    snapping mechanism

--- tests/test_mechanisms.py
+++ tests/test_mechanisms.py
@@ -6,19 +6,48 @@ import opendp.whitenoise.core as wn
 from tests import TEST_CSV_PATH, test_csv_names
 
 
+def dp_all(numeric, categorical, args):
+    return {
+        "covariance": wn.dp_covariance(left=numeric, right=numeric, **args),
+        "histogram": wn.dp_histogram(categorical, **args),
+        "maximum": wn.dp_maximum(numeric, **args),
+        "mean": wn.dp_mean(numeric, **args),
+        "median": wn.dp_median(numeric, **args),
+        "minimum": wn.dp_minimum(numeric, **args),
+        "quantile": wn.dp_quantile(numeric, .75, **args),
+        "raw_moment": wn.dp_raw_moment(numeric, 2, **args),
+        "sum": wn.dp_sum(numeric, **args),
+        "variance": wn.dp_variance(numeric, **args)
+    }
+
+def dp_all_snapping(numeric, categorical, args):
+    return {
+        # "covariance": wn.dp_covariance(left=numeric, right=numeric, **args),
+        # "histogram": wn.dp_histogram(categorical, **args),
+        "maximum": wn.dp_maximum(numeric, **args),
+        "mean": wn.dp_mean(numeric, **args),
+        "median": wn.dp_median(numeric, **args),
+        "minimum": wn.dp_minimum(numeric, **args),
+        "quantile": wn.dp_quantile(numeric, .75, **args),
+        # "raw_moment": wn.dp_raw_moment(numeric, 2, **args),
+        "sum": wn.dp_sum(numeric, **args),
+        "variance": wn.dp_variance(numeric, **args)
+    }
+
+
 def analytic_gaussian_similarity():
-    an_gauss = []
-    gauss = []
-    for i in range(100):
-        with wn.Analysis(strict_parameter_checks=False):
-            PUMS = wn.Dataset(path=TEST_CSV_PATH, column_names=test_csv_names)
+    analytic_gauss_estimates = []
+    gauss_estimates = []
+    with wn.Analysis(strict_parameter_checks=False):
+        PUMS = wn.Dataset(path=TEST_CSV_PATH, column_names=test_csv_names)
 
-            age = wn.impute(
-                wn.to_float(PUMS['age']),
-                data_lower=0.,
-                data_upper=100.,
-                data_rows=1000)
+        age = wn.impute(
+            wn.to_float(PUMS['age']),
+            data_lower=0.,
+            data_upper=100.,
+            data_rows=1000)
 
+        for i in range(100):
             an_gauss_component = wn.dp_mean(
                 age, mechanism="AnalyticGaussian",
                 privacy_usage={"epsilon": 1.0, "delta": 1E-6})
@@ -26,29 +55,77 @@ def analytic_gaussian_similarity():
                 age, mechanism="Gaussian",
                 privacy_usage={"epsilon": 1.0, "delta": 1E-6})
 
-            an_gauss.append(an_gauss_component.value)
-            gauss.append(gauss_component.value)
+            # this triggers an analysis.release (which also computes gauss_component)
+            analytic_gauss_estimates.append(an_gauss_component.value)
+            gauss_estimates.append(gauss_component.value)
 
-    print(sum(an_gauss) / len(an_gauss))
-    print(sum(gauss) / len(gauss))
+    print(sum(analytic_gauss_estimates) / len(analytic_gauss_estimates))
+    print(sum(gauss_estimates) / len(gauss_estimates))
+
+
+def snapping_similarity():
+    snapping_estimates = []
+    laplace_estimates = []
+    with wn.Analysis(strict_parameter_checks=False):
+        PUMS = wn.Dataset(path=TEST_CSV_PATH, column_names=test_csv_names)
+
+        age = wn.impute(
+            wn.to_float(PUMS['age']),
+            data_lower=0.,
+            data_upper=100.,
+            data_rows=1000)
+
+        for i in range(100):
+            snapping_component = wn.dp_mean(
+                age, mechanism="snapping",
+                privacy_usage={"epsilon": 1.0, "delta": 1E-6})
+            laplace_component = wn.dp_mean(
+                age, mechanism="laplace",
+                privacy_usage={"epsilon": 1.0, "delta": 1E-6})
+
+            snapping_estimates.append(snapping_component.value)
+            laplace_estimates.append(laplace_component.value)
+
+    print(sum(snapping_estimates) / len(snapping_estimates))
+    print(sum(laplace_estimates) / len(laplace_estimates))
+
 
 @pytest.mark.parametrize(
-    "mechanism,privacy_usage",
+    "args,constructor",
     [
-        pytest.param("AnalyticGaussian", {"epsilon": 2.0, "delta": 1E-6}, id="AnalyticGaussian"),
-        pytest.param("Gaussian", {"epsilon": 1.0, "delta": 1E-6}, id="Gaussian"),
-        pytest.param("Laplace", {"epsilon": 2.0}, id="Laplace"),
+        pytest.param({
+            "mechanism": "AnalyticGaussian",
+            "privacy_usage": {"epsilon": 2.0, "delta": 1E-6}
+        }, dp_all, id="AnalyticGaussian"),
+        pytest.param({
+            "mechanism": "Gaussian",
+            "privacy_usage": {"epsilon": 1.0, "delta": 1E-6}
+        }, dp_all, id="Gaussian"),
+        pytest.param({
+            "mechanism": "Laplace",
+            "privacy_usage": {"epsilon": 2.0, "delta": 1E-6}
+        }, dp_all, id="Laplace"),
+        pytest.param({
+            "mechanism": "Snapping",
+            "privacy_usage": {"epsilon": 2.0, "delta": 1E-6}
+        }, dp_all_snapping, id="Snapping"),
 
         pytest.param(
-            "Gaussian", {"epsilon": 2.0, "delta": 1E-6},
+            {
+                "mechanism": "Gaussian",
+                "privacy_usage": {"epsilon": 1.0, "delta": 1E-6}
+            },
+            dp_all,
             id="Gaussian_large_epsilon",
             marks=pytest.mark.xfail(raises=AssertionError)),
     ],
 )
-def test_mechanism(mechanism, privacy_usage):
+def test_mechanism(args, constructor):
     with wn.Analysis() as analysis:
         PUMS = wn.Dataset(path=TEST_CSV_PATH, column_names=test_csv_names)
-        categorical = wn.to_bool(PUMS['sex'], "1")
+        categorical = wn.resize(
+            wn.clamp(PUMS['sex'], categories=["0", "1"], null_value="0"),
+            number_rows=1000)
 
         numeric = wn.impute(
             wn.to_float(PUMS['age']),
@@ -56,7 +133,7 @@ def test_mechanism(mechanism, privacy_usage):
             data_upper=100.,
             data_rows=1000)
 
-        all = dp_all(numeric, categorical, privacy_usage, mechanism)
+        all = constructor(numeric, categorical, args)
 
         analysis.release()
         all_values = {stat: all[stat].value for stat in all}
@@ -65,40 +142,3 @@ def test_mechanism(mechanism, privacy_usage):
 
         for value in all_values.values():
             assert value is not None
-
-
-def dp_all(numeric, categorical, usage, mechanism):
-    return {
-        "covariance": wn.dp_covariance(
-            left=numeric, right=numeric,
-            mechanism=mechanism,
-            privacy_usage=usage),
-        "histogram": wn.dp_histogram(
-            categorical,
-            mechanism=mechanism,
-            privacy_usage=usage),
-        "maximum": wn.dp_maximum(
-            numeric, mechanism=mechanism,
-            privacy_usage=usage),
-        "mean": wn.dp_mean(
-            numeric, mechanism=mechanism,
-            privacy_usage=usage),
-        "median": wn.dp_median(
-            numeric, mechanism=mechanism,
-            privacy_usage=usage),
-        "minimum": wn.dp_minimum(
-            numeric, mechanism=mechanism,
-            privacy_usage=usage),
-        "quantile": wn.dp_quantile(
-            numeric, .75, mechanism=mechanism,
-            privacy_usage=usage),
-        "raw_moment": wn.dp_raw_moment(
-            numeric, 2, mechanism=mechanism,
-            privacy_usage=usage),
-        "sum": wn.dp_sum(
-            numeric, mechanism=mechanism,
-            privacy_usage=usage),
-        "variance": wn.dp_variance(
-            numeric, mechanism=mechanism,
-            privacy_usage=usage)
-    }
