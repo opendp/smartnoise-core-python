b2c7c54ad7c81d59af1a1405a181e0ceec8bf641
ecowan <ethan24@gmail.com>
2021-02-08 12:22:32 -0500

    Removing error message comment

--- opendp/smartnoise/transfer/dp_gradient_selector.py
+++ opendp/smartnoise/transfer/dp_gradient_selector.py
@@ -26,10 +26,6 @@ class DPGradientSelector(object):
         dists = [(i, torch.norm(torch.abs(x-mean))) for i, x in enumerate(self.tensor_list)]
         return sorted(dists, key=lambda x: x[1])
 
-    # Currently throwing the following error:
-    # "Error: at node_id 5
-    #  Caused by: node specification Impute(Impute):
-    #  Caused by: lower is greater than upper"
     def select_gradient_tensor(self):
         """
         Use DP Exponential mechanism to select a gradient
